363092707b397f2f2089404a0b64abe0f381891d
lagrandecode <seunogunmolufirst1@gmail.com>
2023-01-09 00:17:15 +0100

    "commit"

--- api/views.py
+++ api/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import ApiModel
+from .serializers import ApiSerializers
 
 # Create your views here.
 
 
-def index(request):
-    return render(request,'home.html')
+@api_view(['GET', 'POST'])
+def api_list(request):
+    "Posting the List of Items"
+    if request.method == 'GET':
+        viewapi = ApiModel.objects.all()
+        serializer = ApiSerializers(viewapi,many=True)
+        return Response(serializer.data)
+
+
+    elif request.method =='POST':
+        serializer = ApiSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
