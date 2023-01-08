f930dbbe5f77e2694ab2b91456e9ac23d784d916
lagrandecode <seunogunmolufirst1@gmail.com>
2023-01-09 00:01:07 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ApiModel(models.Model):
+    name = models.CharField(max_length=100)
+    actual_price = models.PositiveIntegerField(default=0)
+    sales_price = models.PositiveIntegerField(default=0)
+    desc = models.CharField(max_length=400)
+    contact = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
