59164dc1d9a1138c59c5996caaeb942449144357
lagrandecode <seunogunmolufirst1@gmail.com>
2023-01-13 22:10:29 +0100

    "commit"

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('snip/',views.api_list,name='api'),
-    path('snip/<int:pk>/',views.api_detail)
+    path('',views.api_list,name='api'),
+    path('<int:pk>/',views.api_detail)
 ]
