e65ddf89c6c96c097d018989190175e0b077d554
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2023-07-18 20:51:16 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -10,6 +10,7 @@ class ApiModel(models.Model):
     desc = models.CharField(max_length=400)
     contact = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
